b1123942da5df75baa32ce5039d580dc5d608ff2
cagedmantis <carlos.amedee@gmail.com>
2014-04-20 11:01:16 -0400

    Cleaning up yasnippet config.

--- configs/config-yasnippet.el
+++ configs/config-yasnippet.el
@@ -1,16 +1,10 @@
 ;; yasnippet
-;; (add-to-list 'load-path
-;; 	     "~/.emacs.d/plugins/yasnippet-0.6.1c")
 
-;; (require 'yasnippet)
-;; (yas/initialize)
+(require 'yasnippet)
 
-(yas/load-directory "~/.emacs.d/modelib/snippets")
+(yas-global-mode 1)
 
-;; (add-to-list yas-snippet-dirs
-;;               "~/.emacs.d/plugins/yasnippet-0.6.1c/snippets") 
-
-;; (setq yas-snippet-dirs (append yas-snippet-dirs
-;;                                '("~/.emacs.d/modelib/snippets")))
+(setq yas-snippet-dirs (append yas-snippet-dirs
+                               '("~/.emacs.d/modelib/snippets")))
 
 (provide 'config-yasnippet)
