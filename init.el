bd4f4e062bb36c60f99cdff237d18ea644ac6330
Carlos Amedee <carlos.amedee@gmail.com>
2014-07-31 06:04:25 -0400

    Changed default font

--- init.el
+++ init.el
@@ -120,8 +120,22 @@ with a Windows external keyboard from time to time."
 ;; doxymacs
 
 ;; Change the font and size
-(set-default-font "DejaVu Sans Mono")
-(set-face-attribute 'default nil :height 121)
+
+(setq preferred-font "Fira Mono")
+;;(setq preferred-font "Source Code Pro")
+
+;; Does a font exist?
+(defun font-existsp (font)
+    (if (null (x-list-fonts font))
+        nil t))
+
+(if (font-existsp preferred-font)
+    (set-default-font preferred-font)
+  (set-default-font "DejaVu Sans Mono"))
+
+;;(set-default-font "DejaVu Sans Mono")
+;;(set-default-font "Source Code Pro")
+(set-face-attribute 'default nil :height 100)
 
 (setq tex-dvi-view-command "xdvi")
 
@@ -151,3 +165,5 @@ with a Windows external keyboard from time to time."
 
 ;; (require 'powerline)
 ;; (powerline-center-evil-theme)
+
+
