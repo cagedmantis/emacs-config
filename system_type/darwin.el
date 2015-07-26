653f3445f4431db857b2691dd671a6c99a53fc7d
Carlos Amedee <carlos.amedee@gmail.com>
2015-07-26 12:17:58 -0400

    Move osx key modifications into darwin configuration file.

--- system_type/darwin.el
+++ system_type/darwin.el
@@ -33,4 +33,33 @@
 
 (setq default-directory "~/")
 
+(defun swap-meta-and-super ()
+  "Swap the mapping of meta and super. Very useful for people using their Mac
+with a Windows external keyboard from time to time."
+  (interactive)
+  (if (eq mac-command-modifier 'super)
+      (progn
+        (setq mac-command-modifier 'meta)
+        (setq mac-option-modifier 'super)
+        (message "Command is now bound to META and Option is bound to SUPER."))
+    (progn
+      (setq mac-command-modifier 'super)
+      (setq mac-option-modifier 'meta)
+      (message "Command is now bound to SUPER and Option is bound to META."))))
+
+(global-set-key (kbd "C-c w") 'swap-meta-and-super)
+
+;;utf-8
+(prefer-coding-system 'utf-8)
+(when (display-graphic-p)
+  (setq x-select-request-type '(UTF8_STRING COMPOUND_TEXT TEXT STRING)))
+
+;;utf-8
+(setq locale-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+
+
 (provide 'darwin)
