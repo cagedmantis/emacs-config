4a60ce645644d89adc3fc519cf288d72daa24d59
Carlos Amedee <carlos.amedee@gmail.com>
2016-10-22 11:31:25 -0400

    Reorganize go settings.

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -1,3 +1,4 @@
+(global-flycheck-mode)
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
 (add-hook 'sh-mode-hook 'flycheck-mode)
--- lisp/init-golint.el
+++
@@ -1,6 +0,0 @@
-(add-to-list 'load-path (concat (getenv "GOPATH")  "/src/github.com/golang/lint/misc/emacs"))
-(require 'golint)
-
-
-
-(provide 'init-golint)
