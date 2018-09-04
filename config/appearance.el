5f321d196d7b00367eafaaef8ad3fa5c391f647e
Carlos Amedee <carlos.amedee@gmail.com>
2018-09-04 14:56:40 -0400

    appearance cleanup.

--- config/appearance.el
+++ config/appearance.el
@@ -9,25 +9,25 @@
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))      ; Disable the menubar
 
 (setq inhibit-startup-message t)               ; No message at startup
-(setq shell-file-name "/bin/bash")             ; Set Shell for M-| command
-(setq sentence-end-double-space nil)           ; Sentences end with one space
-(setq-default indent-tabs-mode nil)            ; Use spaces instead of tabs
 (column-number-mode t)                         ; Show column number in mode-line
-(global-font-lock-mode t)		               ; fonts are automatically highlighted
-(setq visible-bell t)                          ; No beep when reporting errors
-(size-indication-mode t)
-
-;;El Capitan fix
-(setq visible-bell nil) ;; The default
-(setq ring-bell-function 'ignore)
-
-(setq ispell-dictionary "english")             ; Set ispell dictionary
-(setq make-backup-files t)                     ; backup files ~
-(show-paren-mode 1)                            ; turn on paren match highlighting
 (line-number-mode 1)                           ; show line number the cursor is on, in status bar (the mode line)
 (global-linum-mode 1)                          ; always show line numbers
+(global-font-lock-mode t)		               ; fonts are automatically highlighted
+(size-indication-mode t)
+(show-paren-mode 1)                            ; turn on paren match highlighting
 (setq linum-format " %d ")                     ; fixes bug where line numbers are not buffered in visual-line-mode
 (global-visual-line-mode 1)                    ; Soft wrap lines
+
+(when window-system
+  (tooltip-mode -1)
+  (blink-cursor-mode -1)
+  (mouse-wheel-mode t)
+  (progn (fringe-mode 5)
+         (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
+         (add-to-list 'default-frame-alist '(ns-appearance . dark))
+           ;;;(add-to-list 'default-frame-alist '(ns-appearance . light))
+         (setq frame-title-format nil)))
+
 
 (set-default 'indicate-empty-lines t)
 (set-default 'imenu-auto-rescan t)
@@ -44,30 +44,9 @@
 (setq xterm-mouse-mode t)
 (ansi-color-for-comint-mode-on)
 
-;; Enable syntax highlighting for older Emacsen that have it off
-(global-font-lock-mode t)
-
-;; Save a list of recent files visited.
-(recentf-mode 1)
-
 ;; Default to unified diffs
 (setq diff-switches "-u -w"
       magit-diff-options "-w")
-
-;; Set window title to file name
-(when window-system
-  (setq frame-title-format '(buffer-file-name "%f" ("%b")))
-  (tooltip-mode -1)
-  (mouse-wheel-mode t)
-  (blink-cursor-mode -1))
-
-;; Backup files go into a backup dir
-(setq backup-by-copying t                                       ; don't clobber symlinks
-	  backup-directory-alist '(("." . "~/.emacs.d/backups"))    ; don't litter my fs tree
-	  delete-old-versions t
-	  kept-new-versions 6
-	  kept-old-versions 2
-	  version-control t)                                        ; use versioned backups
 
 ;; disable line mode for listed modes
 (setq linum-disabled-modes-list '(shell-mode ansi-term term-mode eshell-mode wl-summary-mode compilation-mode fundamental-mode))
@@ -84,11 +63,6 @@
 
 ;; Don't defer screen updates when performing operations
 (setq redisplay-dont-pause t)
-
-(when window-system
-  (setq frame-title-format '(buffer-file-name "%f" ("%b")))
-  (tooltip-mode -1)
-  (blink-cursor-mode -1))
 
 ;; Unclutter the modeline
 (use-package diminish
@@ -125,9 +99,6 @@
   :config
   (hlinum-activate))
 
-;; Show current function in modeline.
-;; (which-function-mode)
-
 ;; Ensure linum-mode is disabled in certain major modes.
 (setq linum-disabled-modes
       '(term-mode slime-repl-mode magit-status-mode help-mode nrepl-mode
@@ -137,6 +108,33 @@
 (defun linum-on ()
   (unless (or (minibufferp) (member major-mode linum-disabled-modes))
     (linum-mode 1)))
+
+;; Indent guides
+(use-package highlight-indent-guides
+  :ensure t
+  :hook (prog-mode . highlight-indent-guides-mode)
+  :config (setq highlight-indent-guides-method 'column))
+
+;; Modeline - hai2nan
+(when window-system
+  (use-package moody
+    :ensure t
+    :config
+    (setq x-underline-at-descent-line t)
+    (setq moody-mode-line-height 24)
+    (setq moody-slant-function #'moody-slant-apple-rgb)
+    (moody-replace-mode-line-buffer-identification)
+    (moody-replace-vc-mode)))
+
+(add-hook 'prog-mode-hook (lambda ()
+                            (interactive)
+                            (setq show-trailing-whitespace 1)))
+
+;; Reduce scroll lag
+;; https://emacs.stackexchange.com/questions/28736/emacs-pointcursor-movement-lag/28746
+(setq auto-window-vscroll nil)
+
+(setq font-lock-maximum-decoration t)
 
 (provide 'appearance)
 
