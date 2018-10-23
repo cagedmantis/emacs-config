879fd41f7d4d5f069d44de179add406198b3bf42
Carlos Amedee <carlos.amedee@gmail.com>
2018-10-23 04:20:19 -0400

    Experiment with new window jumping.

--- lisp/init-switch-window.el
+++ lisp/init-switch-window.el
@@ -1,7 +1,22 @@
+;;; init-switch-window.el --- switch-window configuration
+
+;;; Commentary:
+
+;;; Code:
+
 (use-package switch-window
   :ensure t
   :config
 
   (global-set-key (kbd "C-x o") 'switch-window))
 
+(use-package ace-window
+  :ensure t
+  :config
+  (global-set-key (kbd "M-o") 'ace-window)
+  (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
+  )
+
 (provide 'init-switch-window)
+
+;;; init-switch-window.el ends here
