2f5bb7400df6baefd48ed0ed58ecb1bf60830ab5
Carlos Amedee <carlos.amedee@gmail.com>
2019-04-03 05:51:08 -0400

    change language server configuration.

--- lisp/init-language-server.el
+++ lisp/init-language-server.el
@@ -12,50 +12,37 @@
                        (direnv-update-environment)
                        (lsp)))
   :config
+  (add-hook 'c++-mode-hook #'lsp)
+  (add-hook 'go-mode-hook #'lsp)
+  (add-hook 'python-mode-hook #'lsp)
+  (add-hook 'rust-mode-hook #'lsp)
   (require 'lsp-clients))
-
-;; (use-package lsp-mode
-;;   :ensure t
-;;   :commands lsp)
-;;   ;; :init
-;;   ;; (setq ...))
-
-;; (use-package lsp-ui
-;;   :ensure t
-;;   :after lsp-mode
-;;   :config
-;;   (setq lsp-ui-doc-header t
-;;         lsp-ui-doc-include-signature t
-;;         lsp-ui-doc-max-width 80
-;;         lsp-ui-doc-max-height 20
-;;         lsp-ui-doc-use-childframe nil))
 
 (use-package lsp-ui
   :ensure t
   :hook (lsp-mode . lsp-ui-mode)
-  :after lsp-mode
+  :after (lsp-mode flycheck)
   :config
-  (setq lsp-ui-sideline-ignore-duplicate t))
-  ;; :config
-  ;; (setq lsp-ui-doc-header t
-  ;;       lsp-ui-doc-include-signature t
-  ;;       lsp-ui-doc-max-width 80
-  ;;       lsp-ui-doc-max-height 20
-  ;;       lsp-ui-doc-use-childframe nil))
-
-
-;; (use-package lsp-ui
-;;   :ensure t
-;;   :commands lsp-ui-mode
-;;   :init
-;;   (add-hook 'lsp-mode-hook 'lsp-ui-mode))
+  (setq lsp-ui-doc-enable t
+		lsp-ui-doc-include-signature t
+		lsp-ui-doc-position 'top
+		lsp-ui-doc-use-childframe t
+		lsp-ui-flycheck-enable t
+		lsp-ui-flycheck-list-position 'right
+		lsp-ui-flycheck-live-reporting t
+		lsp-ui-peek-enable t
+		lsp-ui-peek-list-width 60
+		lsp-ui-peek-peek-height 25
+		;;lsp-ui-sideline-enable nil
+		lsp-ui-sideline-ignore-duplicate t))
 
 (use-package company-lsp
   :ensure t
   :after (company lsp-mode)
   :commands company-lsp
   :config
-  (setq company-lsp-cache-candidates 'auto
+  (setq company-lsp-cache-candidates nil
+		;;company-lsp-cache-candidates 'auto
 		company-lsp-async t
 		company-lsp-enable-recompletion t)
 
