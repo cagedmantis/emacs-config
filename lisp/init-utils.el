9b39ea72f6074c908931d7101a696729de491319
Carlos Amedee <carlos.amedee@gmail.com>
2019-06-09 12:23:04 -0400

    Disable expand region.

--- lisp/init-utils.el
+++ lisp/init-utils.el
@@ -4,11 +4,12 @@
 
 ;;; Code:
 
-(use-package expand-region
-  :ensure t
-  :config
-  ;;(global-set-key (kbd "C-=") 'er/expand-region))
-  (global-set-key (kbd "@") 'er/expand-region))
+;; TODO: revisit enabling expand region
+;; (use-package expand-region
+;;   :ensure t
+;;   :config
+;;   ;;(global-set-key (kbd "C-=") 'er/expand-region))
+;;   (global-set-key (kbd "@") 'er/expand-region))
 
 (use-package restart-emacs
   :ensure t)
