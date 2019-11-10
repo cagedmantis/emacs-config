26210c7c2ad25aba9129104d660aad040c8ca597
Carlos Amedee <carlos.amedee@gmail.com>
2019-11-10 17:43:42 -0500

    Update go tools used. Remove unused goimports argument.

--- lisp/lang-go.el
+++ lisp/lang-go.el
@@ -11,7 +11,6 @@
 
   ;;(setq gofmt-command "gofumpt")
   (setq gofmt-command "goimports")
-  (setq gofmt-args '("-local" "do"))
 
   (defun my-go-mode-hook ()
 	(subword-mode t)
@@ -84,28 +83,29 @@
 ;; modified from github.com/dougm/go-projectile
 
 (defvar go-tools
-  '((errcheck      . "github.com/kisielk/errcheck")
-	(asmfmt        . "github.com/klauspost/asmfmt/cmd/asmfmt")
+  '((asmfmt        . "github.com/klauspost/asmfmt/cmd/asmfmt")
+	(errcheck      . "github.com/kisielk/errcheck")
 	(fillstruct    . "github.com/davidrjenni/reftools/cmd/fillstruct")
-	(gocode        . "github.com/mdempsky/gocode")
+	(gocode        . "github.com/stamblerre/gocode")
 	(godef         . "github.com/rogpeppe/godef")
 	(godoc         . "golang.org/x/tools/cmd/godoc")
 	(goflymake     . "github.com/dougm/goflymake")
-	(gogetdoc      . "github.com/zmb3/gogetdoc")
 	(gogetdoc      . "github.com/zmb3/gogetdoc")
 	(goimports     . "golang.org/x/tools/cmd/goimports")
 	(golint        . "github.com/golang/lint/golint")
 	(gomodifytags  . "github.com/fatih/gomodifytags")
 	(gomvpkg       . "golang.org/x/tools/cmd/gomvpkg")
+	(gopls         . "golang.org/x/tools/gopls@latest") ;; enable modules
 	(gorename      . "golang.org/x/tools/cmd/gorename")
 	(gotags        . "github.com/jstemmer/gotags")
 	(gotests       . "github.com/cweill/gotests")
 	(gounconvert   . "github.com/mdempsky/unconvert")
 	(guru          . "golang.org/x/tools/cmd/guru")
 	(impl          . "github.com/josharian/impl")
-	(bingo         . "github.com/saibing/bingo")
 	(tools         . "honnef.co/go/tools"))
   "Import paths for My Go tools.")
+
+;; TODO: consider adding a way to set GO111MODULE=on for individual commands.
 
 (defun go-get-tools ()
   "Install go related tools via go get."
@@ -116,7 +116,7 @@
       (message "Go tool %s: %s" (car tool) cmd))))
 
 (defun go-update-tools ()
-  "Update go related tools."
+  "Update go related tools via go get."
   (interactive)
   (go-get-tools))
 
