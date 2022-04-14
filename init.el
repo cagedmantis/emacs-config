3a7ef8aeeaac4f4a6b4935d7801f9134ef62ef38
Carlos Amedee <carlos.amedee@gmail.com>
2022-04-14 03:40:06 -0400

    all: remove performance tracking on init

--- init.el
+++ init.el
@@ -6,14 +6,6 @@
 
 ;;(package-initialize)
 
-(add-hook 'emacs-startup-hook
-          (lambda ()
-            (message "Emacs ready in %s with %d garbage collections."
-                     (format "%.2f seconds"
-                             (float-time
-                              (time-subtract after-init-time before-init-time)))
-                     gcs-done)))
-
 (setq dotfiles-dir (file-name-directory
                     (or (buffer-file-name) load-file-name)))
 
