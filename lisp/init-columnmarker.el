8f6ee2f3eb909da0741284a2a34b781c9e3da949
Carlos Amedee <carlos.amedee@gmail.com>
2022-11-23 11:33:35 -0500

    remove unused modules

--- lisp/init-columnmarker.el
+++
@@ -1,4 +0,0 @@
-(use-package fill-column-indicator
-  :ensure t)
-
-(provide 'init-columnmarker)
--- lisp/init-iedit.el
+++
@@ -1,7 +0,0 @@
-(use-package iedit
-  :ensure t
-  :config
-
-  (define-key global-map (kbd "C-c ;") 'iedit-mode))
-
-(provide 'init-iedit)
