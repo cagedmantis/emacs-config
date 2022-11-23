4139cef4cebaa3cd148147c68ccb11a11b54e399
Carlos Amedee <carlos.amedee@gmail.com>
2022-11-23 11:50:54 -0500

    cleanup package init

--- lisp/init-package.el
+++ lisp/init-package.el
@@ -9,11 +9,11 @@
 (unless (assoc-default "melpa" package-archives)
   (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t))
 (unless (assoc-default "melpa-stable" package-archives)
-  (add-to-list 'package-archives '("melpa" . "https://stable.melpa.org/packages/") t))
+  (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t))
 (unless (assoc-default "nongnu" package-archives)
   (add-to-list 'package-archives '("nongnu" . "https://elpa.nongnu.org/nongnu/") t))
 (when (< emacs-major-version 24)
-  (unless (assoc-default "nongnu" package-archives)
+  (unless (assoc-default "gnu" package-archives)
 	(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))))
 
 (setq package-user-dir "~/.emacs.d/packages")
@@ -23,25 +23,8 @@
 (when (not package-archive-contents)
   (package-refresh-contents))
 
-(defvar my-packages '(use-package)
-  "A list of packages to ensure are installed at launch.")
-
-;;grabbed from Emacs Prelude
-;; TODO eliminate the use of cl-lib
-(defun my-packages-installed-p ()
-  (cl-loop for p in my-packages
-        when (not (package-installed-p p)) do (cl-return nil)
-        finally (cl-return t)))
-
-(unless (my-packages-installed-p)
-  ;; check for new packages (package versions)
-  (message "%s" "Emacs is now refreshing its package database...")
-  (package-refresh-contents)
-  (message "%s" " done.")
-  ;; install the missing packages
-  (dolist (p my-packages)
-    (when (not (package-installed-p p))
-      (package-install p))))
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
 
 (use-package auto-package-update
   :ensure t
