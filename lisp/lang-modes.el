3fc093de1876da6d660c2e452cc95ec9be7a0642
Carlos Amedee <carlos@amedee.net>
2024-03-01 20:25:38 -0500

    all: more cleanup

--- lisp/lang-modes.el
+++ lisp/lang-modes.el
@@ -10,26 +10,26 @@
   :config
   (defconst my-protobuf-style
     '((c-basic-offset . 2)
-	  (tab-width . 8)
+      (tab-width . 8)
       (indent-tabs-mode t)))
 
   (add-hook 'protobuf-mode-hook
-			(lambda () (c-add-style "my-style" my-protobuf-style t))))
+	    (lambda () (c-add-style "my-style" my-protobuf-style t)))
+  (require 'protobuf-mode))
 
-(use-package terraform-mode
-  :ensure t)
+;; (use-package terraform-mode
+;;   :ensure t)
 
 (use-package dockerfile-mode
   :ensure t
   :config
   (add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode)))
 
-(use-package haskell-mode
-  :ensure t)
+;; (use-package haskell-mode
+;;   :ensure t)
 
-;; https://jblevins.org/projects/markdown-mode/
 (use-package markdown-mode
-  :ensure edit-indirect
+  :ensure t
   :mode (("README\\.md\\'" . gfm-mode)
          ("\\.md\\'" . markdown-mode)
          ("\\.markdown\\'" . markdown-mode))
@@ -41,16 +41,16 @@
   (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
   (add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode)))
 
-(use-package flycheck-yamllint
-  :ensure t
-  :defer t
-  :init
-  (progn
-    (eval-after-load 'flycheck
-      '(add-hook 'flycheck-mode-hook 'flycheck-yamllint-setup))))
+;; (use-package flycheck-yamllint
+;;   :ensure t
+;;   :defer t
+;;   :init
+;;   (progn
+;;     (eval-after-load 'flycheck
+;;       '(add-hook 'flycheck-mode-hook 'flycheck-yamllint-setup))))
 
-(use-package bazel
-  :ensure t)
+;; (use-package bazel
+;;   :ensure t)
 
 (use-package racket-mode
   :ensure t)
